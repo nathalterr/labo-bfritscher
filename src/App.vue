32c73061e0291091fcdc76069b375e6680173e6b
Boris Fritscher <boris@fritscher.ch>
2025-04-15 13:53:05 +0200

    donation list a supprimer

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <h1>Donations</h1>
+        <input v-model.number="amount" type="number" />
+        <button v-on:click="addAmount" v-bind:disabled="amount <= 0">add</button>
+        <p v-if="amount != '' && amount <= 0" class="bg-danger text-white mt-3 p-2">Attention...</p>
+        <ul>
+            <li v-for="(d, index) in donations" v-bind:key="index">
+                {{ d }} <btn class="btn btn-danger btn-sm" v-on:click="removeDonation(index)">x</btn>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            amount: "",
+            donations: [1234, 34, 234, 234]
+        };
+    },
+    methods: {
+        addAmount() {
+            this.donations.push(this.amount);
+            this.amount = "";
+        },
+        removeDonation(index) {
+            this.donations.splice(index, 1);
+        }
+    }
+};
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import "bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+import "bootswatch/dist/sketchy/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
+
+let myvar = 'Hello World'
+
